488da7048152bc9c1a9b18d0a63bbe9983512fc3
Duy2310 <duyn1@cpp.edu>
2025-02-14 00:26:19 -0800

    Update vector.cpp

--- Lab8/vector.cpp
+++ Lab8/vector.cpp
@@ -6,57 +6,58 @@ using namespace std;
 /**
  * @brief Prints the elements in the vector and their memory locations
  *
- * @param v - vector of integers (passed by const reference to prevent modification)
+ * @param v - vector of integers
  */
 void printMemVec(const vector<int>& v) {
     for (size_t i = 0; i < v.size(); i++) {
-        cout << "Element: " << v[i] << " at Address: " << &v[i] << endl;
+        cout<< "Element: " << v[i] << " at Address: " << &v[i] << endl;
     }
 }
 
 /**
  * @brief Increments all of the elements in v by 10
  *
- * @param v - reference to a vector of integers
+ * @param v - address to a vector of integers
  */
 void incVecBy10(vector<int>& v) {
+    
     for (size_t i = 0; i < v.size(); i++) {
         v[i] += 10;
     }
 }
 
 int main() {
-    // Define a constant integer for the vector size
+    // Create a constant integer called SIZE that is of value 5
     const int SIZE = 5;
 
-    // Declare a vector of integers
+    // Create a vector of integers called vec that can hold up to 5 elements
     vector<int> vec;
 
-    // Populate vec with values from 100 to 104
+    // Use a for loop to populate vec with the values 100 to 104
     for (int i = 100; i < 100 + SIZE; i++) {
         vec.push_back(i);
     }
-
-    cout << "Before Increment------------" << endl;
+    
+    cout << "Before Increment------------\n";
     printMemVec(vec);
 
-    // Increment vector elements by 10
+    // Call incVecBy10(...) on vec
     incVecBy10(vec);
 
-    cout << "After Increment------------" << endl;
+    printf("After Increment------------\n");
     printMemVec(vec);
 
-    // Remove the last element
+    // Remove last element of vec
     vec.pop_back();
 
-    cout << "After Pop------------" << endl;
+    printf("After Pop------------\n");
     printMemVec(vec);
 
     // Append 101 and 102 at the end of vec
     vec.push_back(101);
     vec.push_back(102);
 
-    cout << "After Push------------" << endl;
+    printf("After Push------------\n");
     printMemVec(vec);
 
     return 0;
