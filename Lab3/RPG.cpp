10cab195f84d37fb35013e5ddf6c6d4cb1644765
Duy2310 <duyn1@cpp.edu>
2025-03-20 14:12:27 -0700

    g

--- Lab3/RPG.cpp
+++ Lab3/RPG.cpp
@@ -101,33 +101,3 @@ void RPG::useSkill(RPG* opponent){
     // Call attack on opponent
     attack(opponent);
 }
-
-
-void displayStats(RPG player1, RPG player2){
-    cout << player1.getName() << ": "<< player1.getHealth() << " "
-         << player2.getName() << ": "<< player2.getHealth() << "\n"; 
-}
-void gameLoop(RPG* player1, RPG* player2) {
-    while (player1->isAlive() && player2->isAlive()) {
-        displayStats(*player1, *player2);
-
-        // Player 1's turn
-        cout << player1->getName() << "'s turn\n";
-        player1->useSkill(player2);
-        cout << "----------------------------------------\n";
-
-        if (!player2->isAlive()) break;
-
-        // Player 2's turn
-        cout << player2->getName() << "'s turn\n";
-        player2->useSkill(player1);
-        cout << "----------------------------------------\n";
-    }
-}
-void displayEnd(RPG player1, RPG player2) {
-    if (player1.isAlive()) {
-        cout << "Wiz defeated NPC! Good game!\n";
-    } else {
-        cout << "NPC defeated Wiz! Good game!\n";
-    }
-}
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -4,6 +4,37 @@
 
 using namespace std;
 
+void displayStats(RPG player1, RPG player2){
+    cout << player1.getName() << ": "<< player1.getHealth() << " "
+         << player2.getName() << ": "<< player2.getHealth() << "\n"; 
+}
+void gameLoop(RPG* player1, RPG* player2) {
+    while (player1->isAlive() && player2->isAlive()) {
+        displayStats(*player1, *player2);
+
+        // Player 1's turn
+        cout << player1->getName() << "'s turn\n";
+        player1->useSkill(player2);
+        cout << "----------------------------------------\n";
+
+        if (!player2->isAlive()) break;
+
+        // Player 2's turn
+        cout << player2->getName() << "'s turn\n";
+        player2->useSkill(player1);
+        cout << "----------------------------------------\n";
+    }
+}
+
+void displayEnd(RPG player1, RPG player2) {
+    if (player1.isAlive()) {
+        cout << "Wiz defeated NPC! Good game!\n";
+    } else {
+        cout << "NPC defeated Wiz! Good game!\n";
+    }
+}
+
+
 int main() {
 
     RPG RPG1; // Default constructor
@@ -44,7 +75,7 @@ int main() {
     RPG p2 = RPG();
 
     gameLoop(&p1, &p2);
-    displayStat(p1, p2);
+    displayEnd(p1, p2);
 
     return 0;
 }
